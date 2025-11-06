e730ef79b0772067d0f79945bdfb271b58d18d5b
Kasula Vanessa Vilhelmiina <vanessa.kasula@outlook.com>
2025-11-06 20:52:24 +0200

    deploy settings

--- my-app/vite.config.ts
+++ my-app/vite.config.ts
@@ -4,20 +4,20 @@ import tailwindcss from '@tailwindcss/vite'
 
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 import { resolve } from 'node:path'
-import { nitroV2Plugin } from '@tanstack/nitro-v2-vite-plugin'
+// import { nitroV2Plugin } from '@tanstack/nitro-v2-vite-plugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    nitroV2Plugin(),
+    // nitroV2Plugin(),
     TanStackRouterVite({ autoCodeSplitting: true }),
     viteReact(),
     tailwindcss(),
   ],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-  },
+  // test: {
+  //   globals: true,
+  //   environment: 'jsdom',
+  // },
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),

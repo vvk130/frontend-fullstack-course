cba6cc033022ae3fe52f09451de152f51ded3dba
Kasula Vanessa Vilhelmiina <vanessa.kasula@outlook.com>
2025-11-25 23:55:47 +0200

    Apiurl

--- my-app/src/apiUrl.ts
+++ my-app/src/apiUrl.ts
@@ -1,3 +1,3 @@
-export const apiUrlWithoutApiWord = 'http://fullstackbackend.runasp.net/';
+export const apiUrlWithoutApiWord = 'https://horsegame.somee.com/';
 export const apiUrl = `${apiUrlWithoutApiWord}api/`; 
 export const frontEndUrl = "https://frontend-fullstck.netlify.app/";
